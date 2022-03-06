4f4cc5e1a9c6b5e3b5d1d43c06201e8d0a6aaa26
Kundan <kundan kamble>
2022-03-06 05:46:59 -0800

    UC1

---
+++ RandomProgram/RandomProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿int Coin_Head = 1;
+Random random = new Random();
+int coincheck = random.Next(0,2);
+if (coincheck == Coin_Head)
+{
+    Console.WriteLine("coin is Head");
+
+}
+else
+{
+    Console.WriteLine("Coin is Tails");
+}
